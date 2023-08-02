6c3fb612fb81420306c6cd5d24d318e56b6c9fdc
沈丽君 <80809788@qq.com>
2023-08-02 15:19:34 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Use kernel 6.1
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/' target/linux/x86/Makefile
+#sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/' target/linux/x86/Makefile
 
 #Use kernel 5.10
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/' target/linux/ramips/Makefile
+#sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/' target/linux/ramips/Makefile
 
 # Modify default theme
 sed -i 's/bootstrap/argon/' feeds/luci/modules/luci-base/root/etc/config/luci
-sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
+#sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/' feeds/luci/collections/luci-light/Makefile
