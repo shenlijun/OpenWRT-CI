8a6f2b2ef06777597581a93750061767ea1ab9fd
沈丽君 <80809788@qq.com>
2023-08-02 15:18:03 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Add ssrp
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 #Add x550
 git clone https://github.com/shenlijun/openwrt-x550-nbase-t package/openwrt-x550-nbase-t
