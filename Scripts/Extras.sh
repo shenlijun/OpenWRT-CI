d3af2769729f340f0be40c4b6726fceada82a3a9
沈丽君 <lijun.shen@cnmail.org>
2025-03-20 13:12:25 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -4,5 +4,5 @@
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 #Add x550
-git clone https://github.com/shenlijun/openwrt-x550-nbase-t package/openwrt-x550-nbase-t
+#git clone https://github.com/shenlijun/openwrt-x550-nbase-t package/openwrt-x550-nbase-t  
 
