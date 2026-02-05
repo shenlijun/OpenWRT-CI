f1269b974ccf52c99c8bddecb07d5e08ffb71bc8
沈丽君 <lijun.shen@cnmail.org>
2026-02-05 16:27:22 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -10,3 +10,16 @@
 sed -i 's/bootstrap/argon/' feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
 sed -i 's/bootstrap/argon/' feeds/luci/collections/luci-light/Makefile
+
+
+##Contents from the master branch
+wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
+#Rust
+tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/rust"
+
+#Xray
+tar -xzf master.tar.gz -C feeds/packages/net/ --strip=2 "packages-master/net/xray-core"
+tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
+
+rm master.tar.gz
+##End of contents from the master branch
