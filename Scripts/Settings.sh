3bfa73cb5e63d35300b4a42e6cb1866cedf8d9de
沈丽君 <lijun.shen@cnmail.org>
2026-03-10 10:19:49 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -16,11 +16,14 @@ sed -i 's/bootstrap/argon/' feeds/luci/collections/luci-light/Makefile
 wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
 
 #Rust
-tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/rust"
+#tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/rust"
 
 #Xray
 tar -xzf master.tar.gz -C feeds/packages/net/ --strip=2 "packages-master/net/xray-core"
 tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
+
+sed -i 's/^PKG_VERSION.*/PKG_VERSION:=26.2.6/' feeds/packages/net/xray-core/Makefile
+sed -i 's/^PKG_HASH.*/PKG_HASH:=a41f170a03fa25d9d39f23f344540b02336a5c893d97b1b837b9477f4b35bc7f/' feeds/packages/net/xray-core/Makefile
 
 rm master.tar.gz
 ##End of contents from the master branch
