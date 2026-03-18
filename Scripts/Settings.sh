6f1626a0213bf4446af4d64d50525b2c6b1ebe37
沈丽君 <lijun.shen@cnmail.org>
2026-03-19 00:28:56 +0800

    Comment out PKG_VERSION and PKG_HASH updates
    Comment out version and hash updates for xray-core

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -23,8 +23,8 @@ sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 tar -xzf master.tar.gz -C feeds/packages/net/ --strip=2 "packages-master/net/xray-core"
 tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
 
-sed -i 's/^PKG_VERSION.*/PKG_VERSION:=26.2.6/' feeds/packages/net/xray-core/Makefile
-sed -i 's/^PKG_HASH.*/PKG_HASH:=a41f170a03fa25d9d39f23f344540b02336a5c893d97b1b837b9477f4b35bc7f/' feeds/packages/net/xray-core/Makefile
+#sed -i 's/^PKG_VERSION.*/PKG_VERSION:=26.2.6/' feeds/packages/net/xray-core/Makefile
+#sed -i 's/^PKG_HASH.*/PKG_HASH:=a41f170a03fa25d9d39f23f344540b02336a5c893d97b1b837b9477f4b35bc7f/' feeds/packages/net/xray-core/Makefile
 
 rm master.tar.gz
 ##End of contents from the master branch
