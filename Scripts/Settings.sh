b83edd545d683981c62c54b9af2c96507cd73b8d
沈丽君 <lijun.shen@cnmail.org>
2026-05-14 18:16:12 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -13,15 +13,17 @@ sed -i 's/bootstrap/argon/' feeds/luci/collections/luci-light/Makefile
 
 
 ##Openwrt-Passwall
-
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
-
 ##End of Openwrt-Passwall
 
+##mosdns
+rm -rf ./feeds/packages/net/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+## End of mosdns
 
 ##Contents from the master branch
 #wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
