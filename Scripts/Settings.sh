eef9f1a5c4dc70f6997dfe102dac91bde9804c94
沈丽君 <lijun.shen@cnmail.org>
2026-09-10 12:20:43 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -26,7 +26,7 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 ## End of mosdns
 
 ##packages from the master branch
-#wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
+wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
 
 #Rust
 #tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/rust"
@@ -34,18 +34,15 @@ sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
 #Xray
 #tar -xzf master.tar.gz -C feeds/packages/net/ --strip=2 "packages-master/net/xray-core"
-#tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
+rm -rf ./feeds/packages/lang/golang
+tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
 #sed -i 's/^PKG_VERSION.*/PKG_VERSION:=26.3.27/' feeds/packages/net/xray-core/Makefile
 #sed -i 's/^PKG_HASH.*/PKG_HASH:=992a4997e6bb846d11469435d687f99ef812fcde1e0a009bb8e95189ea20331d/' feeds/packages/net/xray-core/Makefile
-
-#rm master.tar.gz
-
 ##End of packages from the master branch
 
 ##kernel from the master branch
-#wget https://github.com/immortalwrt/immortalwrt/archive/refs/heads/master.tar.gz
 #rm -rf target/linux
 #tar -xzf master.tar.gz -C target/ --strip=2 "immortalwrt-master/target/linux"
-#rm master.tar.gz
-
 ##End of kernel from the master branch
+
+rm master.tar.gz
