4877edd0173636322c5980cae64ff5b5039c3449
沈丽君 <lijun.shen@cnmail.org>
2026-09-17 23:52:42 +0800

    Update Settings.sh to manage Golang package extraction
    Uncomment commands to remove Golang package and extract files from master.tar.gz. Comment out directory creation and symlink commands.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -34,8 +34,8 @@ wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
 
 #Xray
 #tar -xzf master.tar.gz -C feeds/packages/net/ --strip=2 "packages-master/net/xray-core"
-#rm -rf ./feeds/packages/lang/golang
-#tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
+rm -rf ./feeds/packages/lang/golang
+tar -xzf master.tar.gz -C feeds/packages/lang/ --strip=2 "packages-master/lang/golang"
 #sed -i 's/^PKG_VERSION.*/PKG_VERSION:=26.3.27/' feeds/packages/net/xray-core/Makefile
 #sed -i 's/^PKG_HASH.*/PKG_HASH:=992a4997e6bb846d11469435d687f99ef812fcde1e0a009bb8e95189ea20331d/' feeds/packages/net/xray-core/Makefile
 ##End of packages from the master branch
@@ -47,6 +47,6 @@ wget https://github.com/immortalwrt/packages/archive/refs/heads/master.tar.gz
 
 rm master.tar.gz
 
-mkdir -p staging_dir/host/bin
-ln -sf /usr/bin/go staging_dir/host/bin/go
-ln -sf /usr/bin/gofmt staging_dir/host/bin/gofmt
+#mkdir -p staging_dir/host/bin
+#ln -sf /usr/bin/go staging_dir/host/bin/go
+#ln -sf /usr/bin/gofmt staging_dir/host/bin/gofmt
